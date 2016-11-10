e272aa7686113478032920aa839d93bfc576673f
MiguelAngelYG <yepez2015@hotmail.com>
2016-11-10 11:51:36 -0500

    Update Desarrollo001.java

--- Desarrollo001/src/desarrollo001/Desarrollo001.java
+++ Desarrollo001/src/desarrollo001/Desarrollo001.java
@@ -17,6 +17,7 @@ public class Desarrollo001 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Edison");
+        //19/11/2016
     }
     
 }
